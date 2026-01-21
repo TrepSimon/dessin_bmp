837f5a2947f91b58748f3517a5cb81f7e41b9305
Simon Trepanier <2492224@bdeb.qc.ca>
2026-01-21 14:14:15 -0500

    ajout de helper general

--- fichier.cpp
+++ fichier.cpp
@@ -14,12 +14,49 @@
 #define help_2_position(nom) std::cout<<nom << " x/y x/y\n{forme} {position(int/int)} {position(int/int)}\n"
 #define help_cercle std::cout<<"cercle x/y r\n{forme} {position(int/int) {rayon(int)}\n"
 #define help_trigo(nom) std::cout<< nom << " x/y r b\n{forme} {position(int/int)} {rayon(int)} {b(int)}\n"
-#define help_equi std::cout<<"triangle_equilateral x/y x/y true/false\n{forme} {position(int/int)} {position(int/int)} {booleen(va vers le haut(true)) sinon false\nLes deux points doivent etre sur la meme ligne\n"
+#define help_equi std::cout<<"triangle_equilateral x/y x/y true/false\n{forme} {position(int/int)} {position(int/int)} {booleen(va vers le haut(true)) sinon false}\nLes deux points doivent etre sur la meme ligne\n"
 #define help_triangle_rec std::cout<<"triangle_rectangle x/y x/y\n{forme} {position(int/int)} {position(int/int)}\nles deux positions sont les deux angles qui ne sont pas a 90\nl'angle a 90 degre est genere vers le bas, a moins de ne pas avoir l'espace\n"
+#define new_line std::cout<<"\n\n"
+
+void dessiner_tout_help() {
+    help_2_position("ligne");
+    new_line;
+
+    help_2_position("rectangle");
+    new_line;
+
+    help_2_position("carre");
+    new_line;
+
+    help_cercle;
+    new_line;
+
+    help_trigo("sinus");
+    new_line;
+
+    help_trigo("cosinus");
+    new_line;
+
+    help_trigo("tangente");
+    new_line;
+
+    help_triangle_rec;
+    new_line;
+
+    help_equi;
+    new_line;
+
+}
 
 void dessiner(bmp* b, Draw* draw, std::vector<Data*>* parametre) {
     Methode methode = parametre->at(0)->methode;
     bool isHelper = methode == HELPER;
+
+    if (isHelper && parametre->size() == 1) {
+        dessiner_tout_help();
+        return;
+    }
+
     Data *param1 = parametre->at(1);
 
 
